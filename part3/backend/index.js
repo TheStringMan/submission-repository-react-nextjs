625b1dc5ac56a3ae16d64c42caecd5f5fe3cf07e
Marco Garino <maruko0798@gmail.com>
2025-06-29 11:58:53 +0200

    portato avanti progetto

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -88,7 +88,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 10000 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
